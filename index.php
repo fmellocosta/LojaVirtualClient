42e97ff9ebff55fd5f1071b09bdc4a104a23e3c8
Felipe Mello <fmellocosta@gmail.com>
2017-03-20 22:38:07 -0300

    Commit inicial, criando estrutura basica para testar API

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+	<head>
+		<title>Loja Virtual</title>
+	</head>
+	
+	<body>
+		<h1>Página Principal</h1>
+		<h3>Escolha a opção</h3>
+		<ul>
+			<li><a href="produto/formProduto.php">Produtos</a></li>
+			<li><a href="pedido/formPedido.php">Pedidos</a></li>
+		</ul>
+	</body>
+
+</html>
---
+++ pedido/formPedido.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+	<head>
+		<title>Loja Virtual</title>
+	</head>
+	
+	<body>
+		<h1>Pedido</h1>
+		<h3>Preencha o formulário</h3>
+	</body>
+
+</html>
